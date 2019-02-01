da7568d2e917af224165e8cbd136b7fe559b7c81
ZH1995 <rwzsjfm@qq.com>
2019-02-01 19:31:55 +0800

    实现运行时配置文件

---
+++ Lonicera/config.php
@@ -0,0 +1,15 @@
+<?php
+$arrConf = array(
+    'mode'              => 'debug',
+    'filter'            => true,
+    'charSet'           => 'utf-8',
+    'defaultApp'        => 'front',
+    'defaultController' => 'index',
+    'defaultAction'     => 'index',
+    'UrlControllerName' => 'c',
+    'UrlActionName'     => 'g',
+    'db'                => array(),
+    'smtp'              => array(),
+);
+
+return $arrConf;
