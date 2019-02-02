a0c7849ee94aad43bb83cdcf4e41733ef2450513
ZH1995 <rwzsjfm@qq.com>
2019-02-02 12:53:01 +0800

    加载全局配置文件

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 define('_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('_SYS_PATH', _ROOT . 'Lonicera' . DIRECTORY_SEPARATOR);
 define('_APP', _ROOT . 'app' . DIRECTORY_SEPARATOR);
-
 require _SYS_PATH . 'Lonicera.php';
+require _SYS_PATH . 'config.php';
 
 $app = new Lonicera;
 $app->run();
